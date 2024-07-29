39789bdd1f4eef0c6608b6cbacc6f44dd69a0731
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2024-07-30 02:46:22 +0300

    feat: add remote image patterns to @nextjs configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ prtocol: "https", hostName: "img.clerk.com" }],
+  },
+};
 
 export default nextConfig;
